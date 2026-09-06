9613bba5ccb3f10032c484e13219ba5564bd710f
testwrgame002-dotcom <testwrgame002@gmail.com>
2026-09-05 23:52:49 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -735,12 +735,6 @@ async def forward_invalid_message(message: discord.Message, reason: str):
 
         original_text = message.content or "(sin texto)"
 
-        content = (
-
-            f"**Autor:** {message.author} (`{message.author.id}`)\n\n"
-            f"**Alert:**\n"
-            f"```{original_text[:1800]}```"
-        )
 
         files = await collect_message_attachments(message)
 
